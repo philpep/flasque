181d4fc306dbf26379fdb2cdfafc595136593874
Philippe Pepiot <phil@philpep.org>
2014-04-29 03:03:02 +0200

    flasque: switch --bind to positional argument

--- flasque/main.py
+++ flasque/main.py
@@ -13,12 +13,13 @@ def server(args):
     from gevent.wsgi import WSGIServer
     monkey.patch_all()
     host, port = args.bind.split(":")
+    print("listen on " + args.bind)
     WSGIServer((host, int(port)), get_app()).serve_forever()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bind", help="bind address",
+    parser.add_argument("bind", nargs="?", help="bind address",
                         default="127.0.0.1:5000")
     args = parser.parse_args()
     server(args)
