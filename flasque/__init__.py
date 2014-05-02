51f146443fab927d2e6ecb605aa2883a7a9d5184
Philippe Pepiot <phil@philpep.org>
2014-05-02 15:54:29 +0200

    Add LICENSE

---
+++ flasque/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf8 -*-
+"""
+    flasque
+    ~~~~~~~
+
+    flasque is a flask application exposing redis based queues and a
+    client API using requests.
+
+    :copyright: (c) 2014 by Philippe Pepiot
+    :license: BSD, see LICENSE for more details
+"""
