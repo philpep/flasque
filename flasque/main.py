493ff0c7fe5ed0e3e934b75430518b07366fdbc6
Philippe Pepiot <phil@philpep.org>
2014-05-11 17:09:17 +0200

    server: add --debug option to runserver

--- flasque/main.py
+++ flasque/main.py
@@ -9,10 +9,11 @@ manager = Manager(app)
 
 
 @manager.command
-def runserver(host="127.0.0.1", port="5000"):
+def runserver(host="127.0.0.1", port="5000", debug=False):
     from gevent import monkey
     from gevent.wsgi import WSGIServer
     monkey.patch_all()
+    app.debug = debug
     print(" * Running on http://" + host + ":" + port + "/")
     WSGIServer((host, int(port)), app).serve_forever()
 
